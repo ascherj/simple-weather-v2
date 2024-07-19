f428fd46598435db70f5f1cc173bbf15e52070f2
Jake Ascher <ascherj@gmail.com>
2024-07-19 16:30:48 -0600

    Add Supabase

--- ghi/src/api/api.ts
+++ ghi/src/api/api.ts
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: `${import.meta.env.VITE_SUPABASE_URL}/functions/v1`,
+  headers: {
+    "Authorization": `Bearer ${import.meta.env.VITE_SUPABASE_ANON_KEY}`
+  }
 });
 
 export default api;
